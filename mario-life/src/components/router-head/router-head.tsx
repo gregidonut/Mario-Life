3c16c2c723782d25d17c1b13e7c0fffb701f5f1a
Gregory Babajide V. Osilaja <gregosilaja@outlook.com>
2023-07-13 11:19:31 +0800

    feat: add a specific font from google fonts api
    i don't really care too much about this but it's always good to know how to do it

--- mario-life/src/components/router-head/router-head.tsx
+++ mario-life/src/components/router-head/router-head.tsx
@@ -5,28 +5,30 @@ import { useDocumentHead, useLocation } from "@builder.io/qwik-city";
  * The RouterHead component is placed inside of the document `<head>` element.
  */
 export const RouterHead = component$(() => {
-  const head = useDocumentHead();
-  const loc = useLocation();
+    const head = useDocumentHead();
+    const loc = useLocation();
 
-  return (
-    <>
-      <title>{head.title}</title>
+    return (
+        <>
+            <title>{head.title}</title>
 
-      <link rel="canonical" href={loc.url.href} />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+            <link rel="canonical" href={loc.url.href} />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
 
-      {head.meta.map((m) => (
-        <meta key={m.key} {...m} />
-      ))}
+            <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Ysabeau+Infant" />
 
-      {head.links.map((l) => (
-        <link key={l.key} {...l} />
-      ))}
+            {head.meta.map((m) => (
+                <meta key={m.key} {...m} />
+            ))}
 
-      {head.styles.map((s) => (
-        <style key={s.key} {...s.props} dangerouslySetInnerHTML={s.style} />
-      ))}
-    </>
-  );
+            {head.links.map((l) => (
+                <link key={l.key} {...l} />
+            ))}
+
+            {head.styles.map((s) => (
+                <style key={s.key} {...s.props} dangerouslySetInnerHTML={s.style} />
+            ))}
+        </>
+    );
 });
