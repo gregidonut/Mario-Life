5392022da00e5a1401a40adc651f12fa871c1d8a
Gregory Babajide V. Osilaja <gregosilaja@outlook.com>
2023-07-13 17:10:29 +0800

    feat: complete the header
    - added a fixed width and height as suggested by qwik
    - added unordered list of links to navigate between pages
    - styled everything by attacking everything with flexbox
    - commented out(for now) some of the unneeded css from the starter
      template

--- mario-life/src/components/starter/header/header.tsx
+++ mario-life/src/components/starter/header/header.tsx
@@ -1,11 +1,30 @@
 import { component$ } from "@builder.io/qwik";
 import styles from "./header.module.css";
+import { Link } from "@builder.io/qwik-city";
 
 export default component$(() => {
     return (
         <header>
-            <img src="/cursed-mario.svg" alt="cursed-mario-image" />
-            <h1>Mario Life</h1>
+            <Link href="/">
+                <div class={styles.wrapper}>
+                    <img
+                        width="37.9"
+                        height="68.5"
+                        class={styles.logo}
+                        src="/cursed-mario.svg"
+                        alt="cursed-mario-image"
+                    />
+                    <h1>Mario Life</h1>
+                </div>
+            </Link>
+            <ul>
+                <li>
+                    <Link href="/about">About</Link>
+                </li>
+                <li>
+                    <Link href="/contact">Contact Us</Link>
+                </li>
+            </ul>
         </header>
     );
 });
