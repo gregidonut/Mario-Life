2d2e32d259d86f2ee1e0a6c24efe5e3b694af20f
Gregory Babajide V. Osilaja <gregosilaja@outlook.com>
2023-07-13 16:13:04 +0800

    feat: added logo asset and usage in the header component
    the take away here is that everything inside the public folder is found
    at the root of the site

--- mario-life/src/components/starter/header/header.tsx
+++ mario-life/src/components/starter/header/header.tsx
@@ -1,10 +1,10 @@
 import { component$ } from "@builder.io/qwik";
 import styles from "./header.module.css";
-import { Link } from "@builder.io/qwik-city";
 
 export default component$(() => {
     return (
         <header>
+            <img src="/cursed-mario.svg" alt="cursed-mario-image" />
             <h1>Mario Life</h1>
         </header>
     );
