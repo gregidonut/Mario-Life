d25b618577d04bf60ee333d22fac1f992051b083
Gregory Babajide V. Osilaja <gregosilaja@outlook.com>
2023-07-13 10:26:22 +0800

    feat: add about and contact pages
    did so by adding routes dirs with index.tsx files in them and them
    some minimal bloat about exporting the default component basically.
    exactly like react so far

---
+++ mario-life/src/routes/about/index.tsx
@@ -0,0 +1,24 @@
+import { component$ } from "@builder.io/qwik";
+
+export default component$(() => {
+    return (
+        <article>
+            <h2>About</h2>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur culpa id laboriosam ratione.
+                Cupiditate illo impedit ipsum labore nisi officia perspiciatis reiciendis reprehenderit sed sequi? Ea
+                facilis nostrum totam voluptates?
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur culpa id laboriosam ratione.
+                Cupiditate illo impedit ipsum labore nisi officia perspiciatis reiciendis reprehenderit sed sequi? Ea
+                facilis nostrum totam voluptates?
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur culpa id laboriosam ratione.
+                Cupiditate illo impedit ipsum labore nisi officia perspiciatis reiciendis reprehenderit sed sequi? Ea
+                facilis nostrum totam voluptates?
+            </p>
+        </article>
+    );
+});
---
+++ mario-life/src/routes/contact/index.tsx
@@ -0,0 +1,16 @@
+import { component$ } from "@builder.io/qwik";
+
+export default component$(() => {
+    console.log("hello");
+
+    return (
+        <article>
+            <h2>Contact Us</h2>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur culpa id laboriosam ratione.
+                Cupiditate illo impedit ipsum labore nisi officia perspiciatis reiciendis reprehenderit sed sequi? Ea
+                facilis nostrum totam voluptates?
+            </p>
+        </article>
+    );
+});
